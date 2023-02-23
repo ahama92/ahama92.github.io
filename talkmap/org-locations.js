d9fea00fc89452881e3b1425fee8f1eda2372e51
Mohammad Zandsalimy <mohammad.91225@gmail.com>
2023-02-22 17:29:58 -0800

    Adds the aiaa talk

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,5 +1,10 @@
 var addressPoints = [
   [
+    "National Harbor, Maryland, United States",
+    38.78352745,
+    -77.0141294675926
+  ],
+  [
     "Hampton, Virginia, United States",
     37.0300969,
     -76.3452057
