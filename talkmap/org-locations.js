3399bbe388daa48e34f3660a975229554cc2c392
Mohammad Zandsalimy <mohammad.91225@gmail.com>
2023-08-15 03:02:06 -0700

    Adds NASA Ames presentation

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,12 +1,17 @@
 var addressPoints = [
   [
     "National Harbor, Maryland, United States",
-    38.78352745,
-    -77.0141294675926
+    38.7831238,
+    -77.0128237
   ],
   [
     "Hampton, Virginia, United States",
     37.0300969,
     -76.3452057
+  ],
+  [
+    "Moffett Field, California, United States",
+    37.41129325,
+    -122.05416980310586
   ]
 ];
