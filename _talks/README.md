925bb2ffb7ddb99db391828ab4a4663d61d97b7b
Mohammad Zandsalimy <mohammad.91225@gmail.com>
2024-07-27 21:36:20 -0700

    Adds aviation 2024 talk

--- _talks/README.md
+++ _talks/README.md
@@ -1,3 +1,3 @@
 - If you add a talk here, and it does not show up on your website, the reason might be because the date is a future date. Check back after the date of the talk and see if it works.
 
-- If you run `talkmap.ipynb`, the map of talks location will get updated.
+- If you run `talkmap.ipynb` or `talkmap.py`, the map of talks location will get updated.
--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -6,17 +6,22 @@ var addressPoints = [
   ],
   [
     "Hampton, Virginia, United States",
-    37.0300969,
-    -76.3452057
+    37.0264414,
+    -76.3442846
   ],
   [
     "Moffett Field, California, United States",
-    37.41139495,
-    -122.05683718624664
+    37.41226585,
+    -122.0577734947316
   ],
   [
     "Orlando, Florida, United States",
     28.5421109,
     -81.3790304
+  ],
+  [
+    "Las Vegas, Nevada, United States",
+    36.1672559,
+    -115.148516
   ]
 ];
